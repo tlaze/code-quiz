5db5be61752694e6025036cf083af05a7d2aba32
Tom Lazore <m>
2021-09-18 15:04:37 -0400

    added event listener when button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
-console.log(document.querySelector(".startQuiz"));
-var startButton = document.querySelector(".startQuiz");
+console.log(document.querySelector(".startButton"));
+var button = document.querySelector(".startButton");
+
+button.addEventListener("click",buttonClicked);
+
+function buttonClicked(){
+    console.log("button clicked");
+}
