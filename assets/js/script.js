03b6306bac93a878e032f76e934fbfd0b8641696
Tom Lazore <m>
2021-09-20 00:12:12 -0400

    added question  buttons to gameplay layout which shows after clicking start quiz button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,39 @@ var button = document.querySelector(".startButton");
 //When the start button is clicked, the startButtonClicked function will run
 button.addEventListener("click",startButtonClicked);
 
+//Links gamePage ID to JS variable
 var showGamePage = document.getElementById('gamePage');
+
+//Hides gamepage on initial start of application
 showGamePage.style.display = 'none';
 
-
-
+var quizQuestions = 
+[
+    {
+    question: 'question 1',
+    choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+    answer: 4
+    },
+    {
+    question: 'question 2',
+    choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+    answer: 3 
+    },
+    {
+    question: 'question 3',
+    choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+    answer: 2 
+    },
+    {
+    question: 'question 4',
+    choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+    answer: 1 
+    },
+]
 
 
 //Hides the starting page once the start quiz button is clicked
 function startButtonClicked(){
-    console.log('button clicked');
     var hideContent = document.getElementById('startingPage');
 
     if(hideContent.style.display ==='none'){
@@ -22,6 +45,13 @@ function startButtonClicked(){
     else{
         showGamePage.style.display = 'block';
         hideContent.style.display = 'none';
+        quizGameplay();
     }
 }
 
+function quizGameplay(){
+    for(var i = 0; i < quizQuestions.length; i++){
+        var question = quizQuestions[i];
+        console.log(question);
+    }
+}
