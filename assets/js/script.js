abe0cfaccd5c57db03974b5a6d0bd5164f97ecaa
Tom Lazore <m>
2021-09-20 13:18:34 -0400

    Taking a couple steps back. Displayed first question using local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //Assigns button variable to the startButton class
-var button = document.querySelector(".startButton");
+var quizButton = document.querySelector(".startButton");
 
 //When the start button is clicked, the startButtonClicked function will run
-//button.addEventListener("click",startButtonClicked);  Uncomment later****
+//quizButton.addEventListener("click",startButtonClicked);  Uncomment later****
 
 //Links gamePage ID to JS variable
 var showGamePage = document.getElementById('gamePage');
@@ -19,29 +19,44 @@ hideContent.style.display = 'none'; //Delete LATER***
 var quizQuestions = 
 [
     {
-    'question': 'question 1',
-    'choice': ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
-    'answer': 4
+    'question': 'question 1 text',
+    'choice': [' q1 choice 1', 'q1  choice 2', 'q1 choice 3', 'q1 choice 4'],
+    'answer': '4'
     },
     {
-    'question': 'question 2',
-    'choice': ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
-    'answer': 3 
+    'question': 'question 2 text',
+    'choice': ['q2 choice 1', 'q2 choice 2', 'q2 choice 3', 'q2 choice 4'],
+    'answer': '3' 
     },
     {
-    'question': 'question 3',
-    'choice': ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
-    'answer': 2 
+    'question': 'question 3 text',
+    'choice': ['q3 choice 1', 'q3 choice 2', 'q3 choice 3', 'q3 choice 4'],
+    'answer': '2' 
     },
     {
-    'question': 'question 4',
-    'choice': ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
-    'answer': 1 
+    'question': 'question 4 text',
+    'choice': ['q4 choice 1', 'q4 choice 2', 'q4 choice 3', 'q4 choice 4'],
+    'answer': '1' 
     },
 ]
 
-console.log(quizQuestions[0].question);//displays the string of question 1 in the array
-console.log(quizQuestions[1].choice[0]);
+//Stores quizQuestions in local storage
+var displayedQuestion = document.getElementById('questionText');
+localStorage.setItem('questions', JSON.stringify(quizQuestions));
+
+//Displays question in #questionText
+var ex = JSON.parse(localStorage.getItem("questions"));
+// console.log(ex[0].question); Consoles the first question
+displayedQuestion.textContent = ex[0].question;
+
+
+
+var displayedChoices = document.getElementsByClassName('choices');
+var correctAnswer = document.querySelectorAll('answer');
+
+
+
+
 
 
 
@@ -63,18 +78,3 @@ console.log(quizQuestions[1].choice[0]);
 //     }
 // }
 
-//Cycles throught each question
-function quizGameplay(){
-    //Displays the first question when the game starts
-    var displayedQuestion = document.getElementById('questionText');
-    displayedQuestion.textContent = quizQuestions[0].question;
-
-    //Displays the first choice inside the button
-    var displayedChoices = document.getElementsByClassName('choices');
-    displayedChoices[0].textContent = quizQuestions[1].choice[0];
-
-}
-
-
-
-quizGameplay();
